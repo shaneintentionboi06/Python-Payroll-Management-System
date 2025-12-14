d91defda4a1074ac896abc59f5cda7d5667f93a8
Isaac Paul <shane.isaacpaul@gmail.com>
2025-12-14 20:17:22 +0530

    Separated Modules rather than Inheritancing everything to a single class

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from TUI import PayrollApp
 from data_management import Fetcher
 from dbtransit import Connection
 
-class Payroll(PayrollApp,Connection,Fetcher):
+class Payroll(PayrollApp):
     def __init__(self,Database, driver_class = None, css_path = None, watch_css = False, ansi_color = False):
-        Connection.__init__(self,Database=Database)
-        self.fetcher = Fetcher(self._database_.cursor())
+        self.database = Connection(Database=Database)
+        self.fetcher = Fetcher(self.database._cursor_)
         super().__init__(driver_class, css_path, watch_css, ansi_color)
 
 if __name__ == "__main__":
